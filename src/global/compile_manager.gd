79a09b8c550ffa0a1c4f7d762ea7062acc74ff64
Bread <61806225+hbread00@users.noreply.github.com>
2026-03-02 17:41:42 +0800

    重构编译设置界面，添加版本检查框并移除冗余标签

--- src/global/compile_manager.gd
+++ src/global/compile_manager.gd
@@ -1,29 +1,19 @@
 extends Node
 
-var mingw_regex: RegEx = RegEx.new()
-var emscripten_regex: RegEx = RegEx.new()
-var android_platform_tools_regex: RegEx = RegEx.new()
-
-func _ready() -> void:
-	mingw_regex.compile(r"version\s+(\d+\.\d+\.\d+)")
-	emscripten_regex.compile(r"\)\s+(\d+\.\d+\.\d+)\s+\(")
-	android_platform_tools_regex.compile(r"Version\s+(\d+\.\d+\.\d+(?:-\d+)?)")
-
-
 func get_python_version() -> String:
 	# python3 --version
 	var output: Array[String] = []
 	if (OS.execute("python3", ["--version"], output) != OK
 		or output.size() == 0):
 		return "?"
-	return output[0].strip_edges()
+	return output[0].strip_edges().replace("Python ", "")
 
 func get_scons_version() -> String:
 	# scons --version
 	var output: Array[String] = []
 	if (OS.execute("scons", ["--version"], output) != OK
 		or output.size() == 0):
-		return "?"
+		return ""
 	return output[0].strip_edges()
 
 func get_dotnet_version() -> String:
@@ -31,11 +21,13 @@ func get_dotnet_version() -> String:
 	var output: Array[String] = []
 	if (OS.execute("dotnet", ["--version"], output) != OK
 		or output.size() == 0):
-		return "?"
+		return ""
 	return output[0].strip_edges()
 
 # MINGW_PREFIX
 func get_mingw_version(custom_path: String) -> String:
+	if OS.get_name() != "Windows":
+		return ""
 	# g++ --version
 	var path: String = custom_path
 	if path != "":
@@ -49,18 +41,20 @@ func get_mingw_version(custom_path: String) -> String:
 	var output: Array[String] = []
 	if (OS.execute(path, ["--version"], output) != OK
 		or output.size() == 0):
-		return "?"
-	return mingw_regex.search(output[0]).get_string(1)
+		return ""
+	return output[0].strip_edges()
 
 func get_vulkan_sdk_version() -> String:
-	return "?"
+	if OS.get_name() != "macOS":
+		return ""
+	return ""
 
 func get_emscripten_version() -> String:
 	var output: Array[String] = []
 	if (OS.execute("emcc", ["--version"], output) != OK
 		or output.size() == 0):
-		return "?"
-	return emscripten_regex.search(output[0]).get_string(1)
+		return ""
+	return output[0].strip_edges()
 
 # JAVA_HOME
 func get_jdk_version(custom_path: String) -> String:
@@ -76,27 +70,27 @@ func get_jdk_version(custom_path: String) -> String:
 	var output: Array[String] = []
 	if (OS.execute(path, ["--version"], output) != OK
 		or output.size() == 0):
-		return "?"
-	return output[0].replace("javac ", "").strip_edges()
+		return ""
+	return output[0].strip_edges()
 
 func get_android_sdk_platform_tools_version(custom_path: String) -> String:
 	var path: String = custom_path if custom_path != "" else OS.get_environment("ANDROID_HOME")
 	if path == "":
-		return "?"
+		return ""
 	var output: Array[String] = []
 	if (OS.execute(path.path_join("platform-tools/adb"), ["--version"], output) != OK
 		or output.size() == 0):
-		return "?"
-	return android_platform_tools_regex.search(output[0]).get_string(1)
+		return ""
+	return output[0].strip_edges()
 
 func get_android_sdk_build_tools_version(custom_path: String) -> String:
 	var path: String = custom_path if custom_path != "" else OS.get_environment("ANDROID_HOME")
 	if path == "":
-		return "?"
+		return ""
 	path = path.path_join("build-tools")
 	var dir: DirAccess = DirAccess.open(path)
 	if dir == null:
-		return "?"
+		return ""
 	dir.list_dir_begin()
 	var valid_versions: Array[String] = []
 	var file_name: String = dir.get_next()
@@ -106,18 +100,17 @@ func get_android_sdk_build_tools_version(custom_path: String) -> String:
 				valid_versions.append(file_name)
 		file_name = dir.get_next()
 	if valid_versions.size() == 0:
-		return "?"
-	valid_versions.sort()
-	return valid_versions[-1]
+		return ""
+	return "\n".join(valid_versions)
 
 func get_android_sdk_platform_version(custom_path: String) -> String:
 	var path: String = custom_path if custom_path != "" else OS.get_environment("ANDROID_HOME")
 	if path == "":
-		return "?"
+		return ""
 	path = path.path_join("platforms")
 	var dir: DirAccess = DirAccess.open(path)
 	if dir == null:
-		return "?"
+		return ""
 	dir.list_dir_begin()
 	var valid_versions: Array[String] = []
 	var file_name: String = dir.get_next()
@@ -127,18 +120,17 @@ func get_android_sdk_platform_version(custom_path: String) -> String:
 				valid_versions.append(file_name)
 		file_name = dir.get_next()
 	if valid_versions.size() == 0:
-		return "?"
-	valid_versions.sort()
-	return valid_versions[-1]
+		return ""
+	return "\n".join(valid_versions)
 
 func get_android_sdk_command_line_tools_version(custom_path: String) -> String:
 	var path: String = custom_path if custom_path != "" else OS.get_environment("ANDROID_HOME")
 	if path == "":
-		return "?"
+		return ""
 	var executable_name: String = "sdkmanager.bat" if OS.get_name() == "Windows" else "sdkmanager"
 	if FileAccess.file_exists(path.path_join("cmdline-tools/latest/bin").path_join(executable_name)):
 		return "latest"
-	return "?"
+	return ""
 
 func get_android_cmake_version(custom_path: String) -> String:
 	var path: String = custom_path if custom_path != "" else OS.get_environment("ANDROID_HOME")
@@ -146,12 +138,12 @@ func get_android_cmake_version(custom_path: String) -> String:
 		var output: Array[String] = []
 		if (OS.execute("cmake", ["--version"], output) != OK
 			or output.size() == 0):
-			return "?"
+			return ""
 		return output[0].strip_edges()
 	path = path.path_join("cmake")
 	var dir: DirAccess = DirAccess.open(path)
 	if dir == null:
-		return "?"
+		return ""
 	dir.list_dir_begin()
 	var valid_versions: Array[String] = []
 	var file_name: String = dir.get_next()
@@ -162,18 +154,17 @@ func get_android_cmake_version(custom_path: String) -> String:
 				valid_versions.append(file_name)
 		file_name = dir.get_next()
 	if valid_versions.size() == 0:
-		return "?"
-	valid_versions.sort()
-	return valid_versions[-1]
+		return ""
+	return "\n".join(valid_versions)
 
 func get_android_ndk_version(custom_path: String) -> String:
 	var path: String = custom_path if custom_path != "" else OS.get_environment("ANDROID_HOME")
 	if path == "":
-		return "?"
+		return ""
 	path = path.path_join("ndk")
 	var dir: DirAccess = DirAccess.open(path)
 	if dir == null:
-		return "?"
+		return ""
 	dir.list_dir_begin()
 	var valid_versions: Array[String] = []
 	var file_name: String = dir.get_next()
@@ -183,6 +174,5 @@ func get_android_ndk_version(custom_path: String) -> String:
 				valid_versions.append(file_name)
 		file_name = dir.get_next()
 	if valid_versions.size() == 0:
-		return "?"
-	valid_versions.sort()
-	return valid_versions[-1]
+		return ""
+	return "\n".join(valid_versions)
--- src/page/setting/compile_container.gd
+++ src/page/setting/compile_container.gd
@@ -5,76 +5,93 @@ signal version_refreshed()
 var check_version_task_id: int = -1
 
 
-var regex: Dictionary[String, RegEx] = {}
-var version_regex: Dictionary[String, String] = {
-	"python": r"(\d+\.\d+\.\d+)",
-	"scons": r"v(\d+\.\d+\.\d+)",
-	"dotnet": r"(\d+\.\d+\.\d+)",
-	"mingw": r"version\s+(\d+\.\d+\.\d+)",
-	"vulkan_sdk": "",
-	"jdk": r"(\d+\.\d+\.\d+)",
-	"emscripten": r"\)\s+(\d+\.\d+\.\d+)\s+\(",
-	"cmake": r"cmake version (\d+\.\d+\.\d+)",
-}
-
 @onready var refresh_button: Button = $RefreshButton
 @onready var mingw_path_line: LineEdit = $GridContainer/MingwContainer/MingwPathLine
+@onready var mingw_path_button: Button = $GridContainer/MingwContainer/MingwPathButton
 @onready var mingw_file_dialog: FileDialog = $GridContainer/MingwContainer/MingwPathButton/MingwFileDialog
 @onready var jdk_path_line: LineEdit = $GridContainer/JDKContainer/JDKPathLine
 @onready var jdk_file_dialog: FileDialog = $GridContainer/JDKContainer/JDKPathButton/JDKFileDialog
 @onready var android_sdk_path_line: LineEdit = $GridContainer/AndroidSDKContainer/AndroidSDKPathLine
 @onready var android_sdk_file_dialog: FileDialog = $GridContainer/AndroidSDKContainer/AndroidSDKPathButton/AndroidSDKFileDialog
 
-@onready var python_version_label: Label = $GridContainer/PythonVersionLabel
-@onready var scons_version_label: Label = $GridContainer/SconsVersionLabel
-@onready var dotnet_version_label: Label = $GridContainer/DotnetVersionLabel
-@onready var mingw_version_label: Label = $GridContainer/MingwVersionLabel
-@onready var vulkan_sdk_version_label: Label = $GridContainer/VulkanSDKVersionLabel
-@onready var emscripten_version_label: Label = $GridContainer/EmscriptenVersionLabel
-@onready var jdk_version_label: Label = $GridContainer/JDKVersionLabel
+@onready var python_check: CheckBox = $GridContainer/PythonCheck
+@onready var scons_check: CheckBox = $GridContainer/SconsCheck
+@onready var dotnet_check: CheckBox = $GridContainer/DotnetCheck
+@onready var mingw_check: CheckBox = $GridContainer/MingwCheck
+@onready var vulkan_sdk_check: CheckBox = $GridContainer/VulkanSDKCheck
+@onready var emscripten_check: CheckBox = $GridContainer/EmscriptenCheck
+@onready var jdk_check: CheckBox = $GridContainer/JDKCheck
 
-@onready var android_platform_tools_version_label: Label = $GridContainer/AndroidPlatformToolsVersionLabel
-@onready var android_build_tools_version_label: Label = $GridContainer/AndroidBuildToolsVersionLabel
-@onready var android_platform_version_label: Label = $GridContainer/AndroidPlatformVersionLabel
-@onready var android_command_line_tools_version_label: Label = $GridContainer/AndroidCommandLineToolsVersionLabel
-@onready var android_cmake_version_label: Label = $GridContainer/AndroidCmakeVersionLabel
-@onready var android_ndk_version_label: Label = $GridContainer/AndroidNDKVersionLabel
+@onready var android_platform_tools_check: CheckBox = $GridContainer/AndroidPlatformToolsCheck
+@onready var android_build_tools_check: CheckBox = $GridContainer/AndroidBuildToolsCheck
+@onready var android_platform_check: CheckBox = $GridContainer/AndroidPlatformCheck
+@onready var android_command_line_tools_check: CheckBox = $GridContainer/AndroidCommandLineToolsCheck
+@onready var android_cmake_check: CheckBox = $GridContainer/AndroidCmakeCheck
+@onready var android_ndk_check: CheckBox = $GridContainer/AndroidNDKCheck
 
 func _ready() -> void:
-	for key: String in version_regex.keys():
-		regex[key] = RegEx.new()
-		if regex[key].compile(version_regex[key]) != OK:
-			regex.erase(key)
+	mingw_path_line.text = Config.mingw_prefix
+	jdk_path_line.text = Config.java_home
+	android_sdk_path_line.text = Config.android_home
 	refresh_button.disabled = true
 	check_version_task_id = WorkerThreadPool.add_task(_check_version_task)
+	if OS.get_name() != "Windows":
+		mingw_path_line.editable = false
+		mingw_path_button.disabled = true
 	
 func _check_version_task() -> void:
-	python_version_label.set_deferred("text",
-		CompileManager.get_python_version())
-	scons_version_label.set_deferred("text",
-		CompileManager.get_scons_version())
-	dotnet_version_label.set_deferred("text",
-		CompileManager.get_dotnet_version())
-	mingw_version_label.set_deferred("text",
-		CompileManager.get_mingw_version(mingw_path_line.text))
-	vulkan_sdk_version_label.set_deferred("text",
-		CompileManager.get_vulkan_sdk_version())
-	emscripten_version_label.set_deferred("text",
-		CompileManager.get_emscripten_version())
-	jdk_version_label.set_deferred("text",
-		CompileManager.get_jdk_version(jdk_path_line.text))
-	android_platform_tools_version_label.set_deferred("text",
-		CompileManager.get_android_sdk_platform_tools_version(android_sdk_path_line.text))
-	android_build_tools_version_label.set_deferred("text",
-		CompileManager.get_android_sdk_build_tools_version(android_sdk_path_line.text))
-	android_platform_version_label.set_deferred("text",
-		CompileManager.get_android_sdk_platform_version(android_sdk_path_line.text))
-	android_command_line_tools_version_label.set_deferred("text",
-		CompileManager.get_android_sdk_command_line_tools_version(android_sdk_path_line.text))
-	android_cmake_version_label.set_deferred("text",
-		CompileManager.get_android_cmake_version(android_sdk_path_line.text))
-	android_ndk_version_label.set_deferred("text",
-		CompileManager.get_android_ndk_version(android_sdk_path_line.text))
+	var python_version: String = CompileManager.get_python_version()
+	python_check.set_deferred("button_pressed",
+		python_version != "")
+	python_check.set_deferred("tooltip_text", python_version)
+	var scons_version: String = CompileManager.get_scons_version()
+	scons_check.set_deferred("button_pressed",
+		scons_version != "")
+	scons_check.set_deferred("tooltip_text", scons_version)
+	var dotnet_version: String = CompileManager.get_dotnet_version()
+	dotnet_check.set_deferred("button_pressed",
+		dotnet_version != "")
+	dotnet_check.set_deferred("tooltip_text", dotnet_version)
+	var mingw_version: String = CompileManager.get_mingw_version(mingw_path_line.text)
+	mingw_check.set_deferred("button_pressed",
+		mingw_version != "")
+	mingw_check.set_deferred("tooltip_text", mingw_version)
+	var vulkan_sdk_version: String = CompileManager.get_vulkan_sdk_version()
+	vulkan_sdk_check.set_deferred("button_pressed",
+		vulkan_sdk_version != "")
+	vulkan_sdk_check.set_deferred("tooltip_text", vulkan_sdk_version)
+	var emscripten_version: String = CompileManager.get_emscripten_version()
+	emscripten_check.set_deferred("button_pressed",
+		emscripten_version != "")
+	emscripten_check.set_deferred("tooltip_text", emscripten_version)
+	var jdk_version: String = CompileManager.get_jdk_version(jdk_path_line.text)
+	jdk_check.set_deferred("button_pressed",
+		jdk_version != "")
+	jdk_check.set_deferred("tooltip_text", jdk_version)
+	var android_platform_tools_version: String = CompileManager.get_android_sdk_platform_tools_version(android_sdk_path_line.text)
+	android_platform_tools_check.set_deferred("button_pressed",
+		android_platform_tools_version != "")
+	android_platform_tools_check.set_deferred("tooltip_text", android_platform_tools_version)
+	var android_build_tools_version: String = CompileManager.get_android_sdk_build_tools_version(android_sdk_path_line.text)
+	android_build_tools_check.set_deferred("button_pressed",
+		android_build_tools_version != "")
+	android_build_tools_check.set_deferred("tooltip_text", android_build_tools_version)
+	var android_platform_version: String = CompileManager.get_android_sdk_platform_version(android_sdk_path_line.text)
+	android_platform_check.set_deferred("button_pressed",
+		android_platform_version != "")
+	android_platform_check.set_deferred("tooltip_text", android_platform_version)
+	var android_command_line_tools_version: String = CompileManager.get_android_sdk_command_line_tools_version(android_sdk_path_line.text)
+	android_command_line_tools_check.set_deferred("button_pressed",
+		android_command_line_tools_version != "")
+	android_command_line_tools_check.set_deferred("tooltip_text", android_command_line_tools_version)
+	var android_cmake_version: String = CompileManager.get_android_cmake_version(android_sdk_path_line.text)
+	android_cmake_check.set_deferred("button_pressed",
+		android_cmake_version != "")
+	android_cmake_check.set_deferred("tooltip_text", android_cmake_version)
+	var android_ndk_version: String = CompileManager.get_android_ndk_version(android_sdk_path_line.text)
+	android_ndk_check.set_deferred("button_pressed",
+		android_ndk_version != "")
+	android_ndk_check.set_deferred("tooltip_text", android_ndk_version)
 	version_refreshed.emit.call_deferred()
 
 func _on_refresh_button_pressed() -> void:
